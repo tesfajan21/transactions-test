28ab50eb8ca20b7c9ed2b6f538fc8a512ded48a7
Yohannes Tewolde <jhon.tesfamariam21@gmail.com>
2024-12-24 16:12:56 +0300

    fix: build error due to nextjs 15 dynamic params

--- src/app/transaction/[id]/page.tsx
+++ src/app/transaction/[id]/page.tsx
@@ -3,17 +3,18 @@ import TransactionDetail from "../../../components/TransactionDetail";
 import { loadWalletData } from "../../../utils";
 
 interface TransactionPageProps {
-  params: {
+  params: Promise<{
     id: string;
-  };
+  }>;
 }
 
 export default async function TransactionPage({
   params,
 }: TransactionPageProps) {
   const walletData = loadWalletData();
+  const { id } = await params;
   const transaction = walletData.transactions.find(
-    (t) => t.id === parseInt(params.id)
+    (t) => t.id === parseInt(id)
   );
 
   if (!transaction) {
